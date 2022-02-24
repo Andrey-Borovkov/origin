4fd842d363179eefa9810189509c20223e33057a
Боровков Андрей Владимирович <BorovkovAV@h-lan.ru>
2022-02-24 11:16:37 +0300

    Correct ex003 ex004

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,3 +1,5 @@
 ﻿int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
 int numberB = new Random().Next(1,10);
-Console.WriteLine(numberA / numberB);
+Console.WriteLine(numberB);
+Console.WriteLine(numberA + numberB);
